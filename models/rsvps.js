4bda937f874586dccd7c502ccf6de545501ae265
Mimi Netto <michnetto249@gmail.com>
2020-09-21 21:12:43 -0400

    added edit added image

--- models/rsvps.js
+++ models/rsvps.js
@@ -4,6 +4,7 @@ const rsvpsSchema = new mongoose.Schema({
     name:  { type: String, required: true },
     from:  { type: String, required: true },
     memory:  { type: String, required: true },
+    img: { type: String, required: false },
     wish:  { type: String, required: true },
     side:  { type: String, required: true },
 }, {timestamps: true});
--- models/seed.js
+++ models/seed.js
@@ -1,6 +1,8 @@
 module.exports = [{
   name:  'Mimi',
   from:  'Weymouth, MA',
-  memory: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  wish: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  memory: 'The day they bought home Elly.',
+  img: '/img/beach.jpg',
+  wish: 'They always have good neighbors.',
+  side: 'both'
 }]
